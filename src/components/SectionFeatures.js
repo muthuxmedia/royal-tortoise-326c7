f31f91429114992796e15d4e9c7a541e815c7f15
Stackbit Code Editor <support@stackbit.com>
2021-03-11 06:55:13 +0000

    SectionFeatures.js: changed

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -24,11 +24,7 @@ export default class SectionFeatures extends React.Component {
                 {_.map(_.get(section, 'features', null), (feature, feature_idx) => (
                 <div key={feature_idx} className="block-item">
                   <div className="grid">
-                    {_.get(feature, 'image', null) && (
-                    <div className="cell block-preview">
-                      <img src={withPrefix(_.get(feature, 'image', null))} alt={_.get(feature, 'image_alt', null)} />
-                    </div>
-                    )}
+                    
                     <div className="cell block-content">
                       <h3 className="block-title underline">{_.get(feature, 'title', null)}</h3>
                       <div className="block-copy">
@@ -39,7 +35,13 @@ export default class SectionFeatures extends React.Component {
                         <CtaButtons {...this.props} actions={_.get(feature, 'actions', null)} />
                       </div>
                       )}
-                    </div>l
+                    </div>
+
+                    {_.get(feature, 'image', null) && (
+                    <div className="cell block-preview">
+                      <img src={withPrefix(_.get(feature, 'image', null))} alt={_.get(feature, 'image_alt', null)} />
+                    </div>
+                    )}
                   </div>
                 </div>
                 ))}
